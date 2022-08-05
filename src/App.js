1592e4a6d3d98e1a85d72484ed6fa35672efd250
Okafor Jeffrey <oka4jeffrey1234@gmail.com>
2022-08-05 06:37:00 +0100

    finally changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+    { id: 'e1', title: "Toilet", amount: 30.1, date: new Date(2021, 2, 21) },
+    { id: 'e2', title: "House Insurance", amount: 34.1, date: new Date(2022, 6, 15) },
+    { id: 'e3', title: "Land", amount: 50.1, date: new Date(2019, 8, 24) },
+    { id: 'e4', title: "Hotel", amount: 10.1, date: new Date(2020, 10, 5) },
+];
 
-
-function App() {
-  const expenses = [
-    { title: "Toilet", amount: 30.1, date: new Date(2021, 2, 21) },
-    { title: "House Insurance", amount: 34.1, date: new Date(2022, 6, 21) },
-    { title: "Land", amount: 50.1, date: new Date(2021, 8, 21) },
-    { title: "Hotel", amount: 10.1, date: new Date(2020, 10, 21) },
-  ];
-
-  const AddExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }; 
+const App = () => {
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-      <NewExpense onAddExpense = {AddExpenseHandler} />
-      <Expenses items={expenses}/> 
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
-
+import { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-const clickHandler = () => {
-  setTitle('Updated!');
-  console.log(title);
-};
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
