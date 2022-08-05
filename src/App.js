bb469ce5b8fa959759cb85c6b539fcc16ddffbf0
Okafor Jeffrey <oka4jeffrey1234@gmail.com>
2022-08-05 06:45:41 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Expenses from "./components/Expenses/Expenses";
 const DUMMY_EXPENSES = [
     { id: 'e1', title: "Toilet", amount: 30.1, date: new Date(2021, 2, 21) },
     { id: 'e2', title: "House Insurance", amount: 34.1, date: new Date(2022, 6, 15) },
-    { id: 'e3', title: "Land", amount: 50.1, date: new Date(2019, 8, 24) },
+    { id: 'e3', title: "Land", amount: 50.1, date: new Date(2019, 8, 10) },
     { id: 'e4', title: "Hotel", amount: 10.1, date: new Date(2020, 10, 5) },
 ];
 
